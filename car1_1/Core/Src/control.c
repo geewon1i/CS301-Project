b20582d8938af41e90bb0a4bb84042cb1817039c
geewon1i <12311926@mail.sustech.edu.cn>
2025-12-15 20:58:20 +0800

    1

--- car1_1/Core/Src/control.c
+++ car1_1/Core/Src/control.c
@@ -19,12 +19,12 @@ void init_motors(void)
     /* 软件 PWM 定时器启动 */
     //HAL_TIM_Base_Start_IT(&htim3);
 
-    /* 硬件 PWM：PC9 / PC7 */
+    /* 硬件 PWM：PC9 / PC7 */   //有问题
     HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
-    //HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
+    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
 
     __HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_2, HALF_PWM);
-    //__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_4, HALF_PWM);
+    __HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_4, HALF_PWM);
 
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_SET);
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2,  GPIO_PIN_SET);
@@ -42,20 +42,20 @@ void init_motors(void)
 
 void forward_half(void)
 {
-	// 右轮前进
+	// 左轮
+	HAL_GPIO_WritePin(IN1_L_GPIO_Port, IN1_L_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(IN2_L_GPIO_Port, IN2_L_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(IN3_L_GPIO_Port, IN3_L_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(IN4_L_GPIO_Port, IN4_L_Pin, GPIO_PIN_SET);
+	// 右轮
 	HAL_GPIO_WritePin(IN1_R_GPIO_Port, IN1_R_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(IN2_R_GPIO_Port, IN2_R_Pin, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(IN3_R_GPIO_Port, IN3_R_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(IN4_R_GPIO_Port, IN4_R_Pin, GPIO_PIN_SET);
 
-	// 打开 TIM8 PWM
-	HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
-
 	__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_2, HALF_PWM);
 	__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_4, HALF_PWM);
 
-	while(1);
 
 
 
@@ -99,13 +99,10 @@ void turn_right_half(void)
 void stop_motors(void)
 {
     /* 硬件 PWM 清零 */
-    //__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_2, 0);
-    //__HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_4, 0);
+    __HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_2, 0);
+    __HAL_TIM_SET_COMPARE(&htim8, TIM_CHANNEL_4, 0);
 
-    /* 软件 PWM 关闭 */
-    //sw_pwm_on = 0;
-    //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_RESET);
-    //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+
 
     /* 方向清零 */
     HAL_GPIO_WritePin(IN1_L_GPIO_Port, IN1_L_Pin, GPIO_PIN_RESET);
