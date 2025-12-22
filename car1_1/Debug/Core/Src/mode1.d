f913396e1d671bfc3765875cd911a8b55bc34e27
RedZombie314 <xueyeyc@outlook.com>
2025-12-22 11:08:48 +0800

    added route planning's bt part

--- car1_1/Debug/Core/Src/mode1.d
+++ car1_1/Debug/Core/Src/mode1.d
@@ -24,7 +24,8 @@ Core/Src/mode1.o: ../Core/Src/mode1.c ../Core/Inc/mode1.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
- ../Core/Inc/tim.h ../Core/Inc/main.h ../Core/Inc/gpio.h
+ ../Core/Inc/tim.h ../Core/Inc/main.h ../Core/Inc/gpio.h \
+ ../Core/Inc/stm32f1xx_it.h
 ../Core/Inc/mode1.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
@@ -55,3 +56,4 @@ Core/Src/mode1.o: ../Core/Src/mode1.c ../Core/Inc/mode1.h \
 ../Core/Inc/tim.h:
 ../Core/Inc/main.h:
 ../Core/Inc/gpio.h:
+../Core/Inc/stm32f1xx_it.h:
